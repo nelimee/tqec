64be47739dc083351fa43c2d8943276e26c6272a
Adrien Suau <adrien.suau+github@protonmail.com>
2024-05-23 08:21:31 +0000

    Removing JSONEncodable from Template hierarchy
    This class was not really useful and will be replaced by pydantic

--- src/tqec/templates/base.py
+++ src/tqec/templates/base.py
@@ -1,66 +1,16 @@
 from __future__ import annotations
 
-import json
 import typing as ty
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from dataclasses import dataclass
 
 import numpy
 
-from tqec.enums import CornerPositionEnum, TemplateRelativePositionEnum
 from tqec.exceptions import TQECException
 from tqec.position import Displacement, Shape2D
 
 
-def _json_encoding_default(obj) -> str | dict | None:
-    """Define additional defaults to transform instances to JSON.
-
-    This function is given as parameter to json.dumps to provide a
-    way to translate the enumerations used by some templates to JSON
-    data.
-
-    Raises:
-        TypeError: if an instance of a unimplemented type is provided as
-            parameter.
-    """
-    if isinstance(obj, CornerPositionEnum):
-        return f"{obj.name}"
-    elif isinstance(obj, TemplateRelativePositionEnum):
-        return f"{obj.name}"
-    raise TypeError(f"Type {type(obj).__name__} is not encodable in JSON")
-
-
-class JSONEncodable(ABC):
-    @abstractmethod
-    def to_dict(self) -> dict[str, ty.Any]:
-        """Returns a dict-like representation of the instance.
-
-        Used to implement to_json.
-        """
-        pass
-
-    def to_json(self, **kwargs) -> str:
-        """Returns a JSON representation of the instance.
-
-        Args:
-            **kwargs: keyword arguments forwarded to the json.dumps function.
-                The "default" keyword argument should NOT be present.
-
-        Returns:
-            the JSON representation of the instance.
-
-        Raises:
-            TQECException: if the "default" key is present in kwargs.
-        """
-        if "default" in kwargs:
-            raise TQECException(
-                f"The 'default' key has been found with value '{kwargs.get('default')}' in the provided kwargs."
-                " 'default' key is prohibited in the public API as it is changed internally."
-            )
-        return json.dumps(self.to_dict(), default=_json_encoding_default, **kwargs)
-
-
-class Template(JSONEncodable):
+class Template:
     def __init__(
         self,
         default_x_increment: int = 2,
